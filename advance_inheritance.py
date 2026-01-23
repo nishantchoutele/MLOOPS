770f312126edb0ce310150beb15c968dfe45d865
nishantchoutele <nishantchoutele@gmail.com>
2026-01-23 21:09:56 +0530

    herarchical inheritance completed

--- advance_inheritance.py
+++ advance_inheritance.py
@@ -46,6 +46,38 @@ child.tell_story() # Charlie tells a story
 child.work()       # Charlie is working 
 child.play()       # Charlie is working 
 
+#heirarchical inheritance
+
+#base class
+class Parent:
+    def __init__(self,name):
+        self.name = name 
+
+    def greet(self):
+        print(f"Hello my name is {self.name}")
+
+#derived class 
+class Child1(Parent):
+
+    def play(self):
+        print(f"{self.name} is playing.")
+
+#derived class 2
+class Child2(Parent):
+
+    def study(self):
+        print(f"{self.name} is studing")
+
+#Create instance for child1 and child2
+child1 = Child1("Dave")
+child2 = Child2("Eve")
+#obj
+child1.greet() # Hello my name is dave
+child1.play()  # Dave is playing 
+
+child2.greet()  #Hello my name is Eve
+child2.study()  #Eve is studing 
+
         
 
          
